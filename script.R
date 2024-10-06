2cb0722c0cccb9232a604000d315f817a4a364a6
Augusta <augustamanchester1@gmail.com/>
2024-10-06 12:28:50 -0400

    HW2 Problem 2

--- script.R
+++ script.R
@@ -16,3 +16,37 @@ map(dt, sd)
 map_df(dt, sqrt)
 
 summary(dt)
+
+#2
+means <- numeric(10000)
+
+for (i in 1:10000) {
+  means[i] <- mean(rexp(5, rate = 1))
+}
+
+hist(means, breaks = 50, main = "Distribution Means", 
+     xlab = "Samples", col = "pink")
+
+meanmap<- map_dbl(1:10000, ~mean(rexp(5, rate = 1)))
+
+hist(meanmap, breaks = 50, main = "Distribution Means", 
+     xlab = "Samples", col = "pink")
+
+repmean<- replicate(10000, mean(rexp(5, rate = 1)))
+
+hist(repmean, breaks = 50, main = "Distribution Means", 
+     xlab = "Samples", col = "pink")
+
+
+twoc<- c(5, 10, 20)
+
+par(mfrow = c(1, 3))
+
+for (size in twoc) {
+  means <- replicate(10000, mean(rexp(size, rate = 1)))
+
+hist(means, breaks = 50, main = "Distribution Means", 
+     xlab = "Samples", col = "pink")}
+
+
+
